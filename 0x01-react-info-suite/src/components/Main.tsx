967d54ba32e6350a07b60ef60cbb4e7a123625b8
amir-kedis <amir.kedis@gmail.com>
2023-10-31 22:21:08 +0200

    feat: main component

--- 0x01-react-info-suite/src/components/Main.tsx
+++ 0x01-react-info-suite/src/components/Main.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Main() {
-  return <h2>MAIN COMPONTENT</h2>;
+  return (
+    <main className="main container">
+      <h1 className="main__title">Fun fact about React</h1>
+      <ul className="main__list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
