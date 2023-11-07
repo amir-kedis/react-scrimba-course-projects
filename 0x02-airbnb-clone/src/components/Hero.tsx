243fca9ab06cbd65abacd5dc9e4792b693fab402
amir-kedis <amir.kedis@gmail.com>
2023-11-07 09:12:57 +0200

    feat: hero component

---
+++ 0x02-airbnb-clone/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero__container">
+        <img className="hero__image" src="/assets/hero.png" alt="hero images" />
+        <h1 className="hero__text">Online Experiences</h1>
+        <p className="hero__p">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
+}
